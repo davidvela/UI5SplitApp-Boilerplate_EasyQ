bcbe7f02a576a05548a6836498f4aa1bd83ec720
david <david.vela.tirado@gmail.com>
2017-02-19 19:48:43 +0100

    Component and Manifest created!

---
+++ WebContent/Component.js
@@ -0,0 +1,23 @@
+//jQuery.sap.declare("ui5eq.app.Component");
+//jQuery.sap.require("sap.ui.model.resource.ResourceModel");
+
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
+	"use strict";
+
+return sap.ui.core.UIComponent.extend("ui5eq.Component", {
+		metadata : {
+			manifest: "json"
+		},
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// used only for this lessons to show the request individually...
+			//this.getModel().setUseBatch(false);
+			// additional initialization can be done here
+		}
+
+	});
+});
